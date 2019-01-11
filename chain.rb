a5befa2b468559b263ea4050746fdab809b992ca
gabriel schiavo <gabriel@lewagon.org>
2019-01-11 11:13:20 -0200

    Setup initialize method

--- chain.rb
+++ chain.rb
@@ -1,5 +1,9 @@
 class Block
 	def initialize(index, timestamp, data, previousHash = '') # Init first block
-		
+		self.index = index;
+    self.timestamp = timestamp;
+    self.data = data;
+    self.previousHash = previousHash;
+    self.hash = self.calculateHash();
 	end 
 end 
